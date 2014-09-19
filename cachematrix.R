90721c71a7cb77397ae5928a77210ac952d94072
ask73 <ask2648@gmail.com>
2014-09-18 20:26:08 -0700

    Rprogramming - Assignment 2 -Final Submission File
    Modified R code for Project Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Created By: AKumar
+## Date : 9/18/2014
+## Version : 1:0
+#######################################################################
 
-## Write a short comment describing this function
+## The following programming script is a R function which enables to 
+## cache time-consuming computations. If the matrix variables are constant,
+## then it makes sense to cache the value of inverse of the matrix one time
+## and then reuse the computed inverse matrix again from the cache without
+## wasting time to recompute
+
+## "makeCacheMatrix" is a function to create a special "Matrix" that can cache
+## its inverse value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(z) m <<- z
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## "cacheSolve" function calculates the inverse of the special "matrix" created
+## by "makeCacheMatrix" function. However, it first checks to see
+## if the inverse matrix already exisit in cache (already computed).
+## if the inverse is residing in cache then it skips the computation and picks
+## up the value from cache, otherwise it will compute the inverse of the
+## matrix
+
+cacheSolve <- function(x = matrix(), ...) {
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinv(m)
+  m
 }
